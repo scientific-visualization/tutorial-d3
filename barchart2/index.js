a8dc50a0db8527111ead71e69a517fa85ce87a75
armandossrecife <armando.sousa@gmail.com>
2019-09-09 17:23:14 -0300

    Criado o 2o. exemplo de Barchart horizontal e com escala

---
+++ barchart2/index.js
@@ -0,0 +1,14 @@
+var data = [30, 86, 168, 281, 303, 565];
+
+//Aplica uma escala aos valores passados em "data"
+var scale = d3.scale.linear()
+  .domain([0, 565])
+  .range([0, 300])
+
+d3.select(".chart")
+  .selectAll("div")
+  .data(data)
+    .enter()
+    .append("div")
+    .style("width", function(d) { return scale(d) + 'px' })
+    .text(function(d) { return '$ ' + d; });
